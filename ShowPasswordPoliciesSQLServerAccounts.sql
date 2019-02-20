bc91e8ec071201f460d0bfdcef08a257f36e1edc
designworld-ca <47788179+designworld-ca@users.noreply.github.com>
2019-02-20 12:47:18 -0500

    Create ShowPasswordPoliciesSQLServerAccounts.sql
    shows when password last set, days till expiration....

---
+++ ShowPasswordPoliciesSQLServerAccounts.sql
@@ -0,0 +1,17 @@
+--Select SQL Server accounts showing password policies in effect
+SELECT SL.name AS LoginName 
+      ,LOGINPROPERTY (SL.name, 'PasswordLastSetTime') AS PasswordLastSetTime 
+      ,LOGINPROPERTY (SL.name, 'DaysUntilExpiration') AS DaysUntilExpiration 
+      ,DATEADD(dd, CONVERT(int, LOGINPROPERTY (SL.name, 'DaysUntilExpiration')) 
+                 , CONVERT(datetime, LOGINPROPERTY (SL.name, 'PasswordLastSetTime'))) AS PasswordExpiration 
+      ,SL.is_policy_checked AS IsPolicyChecked 
+      ,LOGINPROPERTY (SL.name, 'IsExpired') AS IsExpired 
+      ,LOGINPROPERTY (SL.name, 'IsMustChange') AS IsMustChange 
+      ,LOGINPROPERTY (SL.name, 'IsLocked') AS IsLocked 
+      ,LOGINPROPERTY (SL.name, 'LockoutTime') AS LockoutTime 
+      ,LOGINPROPERTY (SL.name, 'BadPasswordCount') AS BadPasswordCount 
+      ,LOGINPROPERTY (SL.name, 'BadPasswordTime') AS BadPasswordTime 
+      ,LOGINPROPERTY (SL.name, 'HistoryLength') AS HistoryLength 
+	  ,is_expiration_checked AS WillExpire
+FROM sys.sql_logins AS SL 
+ORDER BY LOGINPROPERTY (SL.name, 'PasswordLastSetTime') DESC
