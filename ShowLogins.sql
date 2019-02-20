0963f2a2873768154b279812d27a8e30ea244e1d
designworld-ca <47788179+designworld-ca@users.noreply.github.com>
2019-02-20 12:42:52 -0500

    ShowLogins.sql
    shows windows and sql server logins and members of some special roles

---
+++ ShowLogins.sql
@@ -0,0 +1,18 @@
+select * from sys.syslogins
+go
+select * from sys.sql_logins 
+go
+sp_helprolemember 'db_securityadmin'
+go
+sp_helprolemember 'db_owner'
+go
+sp_helprolemember 'db_accessadmin'
+go
+sp_helpsrvrolemember 'sysadmin'
+go
+sp_helpsrvrolemember 'serveradmin'
+go
+sp_helpsrvrolemember 'securityadmin'
+go
+EXEC sp_helprotect 'application rolename'
+go
