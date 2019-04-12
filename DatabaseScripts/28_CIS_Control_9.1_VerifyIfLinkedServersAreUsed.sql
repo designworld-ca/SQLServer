35622638a03fc3bbec6c559561977b1c62ec5a2b
designworld-ca <47788179+designworld-ca@users.noreply.github.com>
2019-04-12 12:22:30 +0000

    Delete 28_CIS_Control_9.1_VerifyIfLinkedServersAreUsed.sql

--- DatabaseScripts/28_CIS_Control_9.1_VerifyIfLinkedServersAreUsed.sql
+++
@@ -1,8 +0,0 @@
---checks CIS control 9.1 Limit open ports, protocols and services
---used to query the linked servers and user logins:
-
-exec sp_linkedservers;
-
-select * from sys.servers;
-
-select * from  sys.linked_logins;
