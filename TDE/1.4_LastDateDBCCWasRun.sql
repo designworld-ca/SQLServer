a16c280e73ef5685e918b53bd2b840fbad4b36e7
kevinsky <47788179+designworld-ca@users.noreply.github.com>
2020-07-21 15:27:03 -0400

    Update 1.4_LastDateDBCCWasRun.sql

--- TDE/1.4_LastDateDBCCWasRun.sql
+++ TDE/1.4_LastDateDBCCWasRun.sql
@@ -1,4 +1,5 @@
 -- source: https://jasonbrimhall.info/2012/11/20/last-known-good-checkdb
+--should not try and implement TDE if a checkdb has not been run recently
 
 CREATE TABLE #temp (
        Id INT IDENTITY(1,1), 
@@ -58,39 +59,3 @@ DROP TABLE #temp
 DROP TABLE #DBCCRes
 GO
  
-/* Now Restore the database */
-DECLARE @BackupPath VARCHAR(256)
-	,@BackupName VARCHAR(50)
-SET @BackupPath = 'C:\Database\Backup\' --replace with valid file path
-SET @BackupName = 'TestB.bak'
- 
-SET @BackupPath = @BackupPath + @BackupName
-/* for this contrived example, i will not take a tail log backup
-and just use replace instead */
- 
-RESTORE DATABASE TestB
-	FROM DISK = @BackupPath
-	WITH REPLACE; 
-GO
- 
-/* Rerun The Boot Page Check */
-CREATE TABLE #temp (
-       Id INT IDENTITY(1,1), 
-       ParentObject VARCHAR(255),
-       [Object] VARCHAR(255),
-       Field VARCHAR(255),
-       [Value] VARCHAR(255)
-)
- 
-INSERT INTO #temp
-EXECUTE ('DBCC Page ( TestB,1,9,3) WITH TABLERESULTS');
- 
-/* You will get two results from the following query */
-SELECT *
-	FROM #temp
-	WHERE Field = 'dbi_dbccLastKnownGood';
- 
-/* TAKE note OF the date returned by the last query */
- 
-DROP TABLE #temp;
-GO
